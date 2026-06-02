14b84008caeb182f1c4e20de4746bf666b0606be
Gobi G <gobi.g2106@gmail.com>
2026-06-03 00:42:50 +0530

    first commit

---
+++ q1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class q1{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        int age = sc.nextInt();
+        System.out.println("My name is " + name );
+        System.out.println("My age is " + age);
+        
+    }
+}
